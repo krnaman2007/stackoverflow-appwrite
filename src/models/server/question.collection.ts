2bb65f0912afdd11f1727b53ad2dc66b9bbbe698
Naman Kumar <krnaman.2007@gmail.com>
2026-06-24 09:32:01 +0530

    Created server models

---
+++ src/models/server/question.collection.ts
@@ -0,0 +1,48 @@
+import { DatabasesIndexType, Permission } from "node-appwrite"
+
+import {db, questionCollection} from "../name"
+import {databases} from "./config"
+import { OrderBy } from "node-appwrite"
+
+
+export default async function createQuestionCollection(){
+    //create collection
+    await databases.createCollection(db,questionCollection,questionCollection,[
+        Permission.read("any"),
+        Permission.read("users"),
+        Permission.create("users"),
+        Permission.update("users"),
+        Permission.delete("users"),
+    ])
+    console.log("Question collection is created")
+
+    //creating attributes and indexes
+    await Promise.all([
+        databases.createStringAttribute(db,questionCollection,"title",100,true),
+        databases.createStringAttribute(db,questionCollection,"content",10000,true),
+        databases.createStringAttribute(db,questionCollection,"authorId",50,true),
+        databases.createStringAttribute(db,questionCollection,"tags",50,true,undefined,true),
+        databases.createStringAttribute(db,questionCollection,"attachmentId",50,false),
+    ])
+    console.log("Questions Attributed created")
+
+    //create index
+    // await Promise.all([
+    //     databases.createIndex(
+    //         db,
+    //         questionCollection,
+    //         "title",
+    //         DatabasesIndexType.Fulltext,
+    //         ["title"],
+    //         [OrderBy.Asc]
+    //     ),
+    //     databases.createIndex(
+    //         db,
+    //         questionCollection,
+    //         "content",
+    //         DatabasesIndexType.Fulltext,
+    //         ["cotent"],
+    //         [OrderBy.Asc]
+    //     )
+    // ])
+}
