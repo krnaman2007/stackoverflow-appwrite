2851ade28955056c55992943f38425f6265a4bbf
Naman Kumar <krnaman.2007@gmail.com>
2026-06-26 15:51:54 +0530

    edited comment collection

--- src/models/server/comment.collection.ts
+++ src/models/server/comment.collection.ts
@@ -1,10 +1,10 @@
 import { Permission } from "node-appwrite";
 import { commentCollection, db } from "../name";
-import { databases } from "./config";
+import { tablesDB } from "./config";
 
 export default async function createCommentCollection() {
     // Creating Collection
-    await databases.createCollection(db, commentCollection, commentCollection, [
+    await tablesDB.createTable(db, commentCollection, commentCollection, [
         Permission.create("users"),
         Permission.read("any"),
         Permission.read("users"),
@@ -15,10 +15,10 @@ export default async function createCommentCollection() {
 
     // Creating Attributes
     await Promise.all([
-        databases.createStringAttribute(db, commentCollection, "content", 10000, true),
-        databases.createEnumAttribute(db, commentCollection, "type", ["answer", "question"], true),
-        databases.createStringAttribute(db, commentCollection, "typeId", 50, true),
-        databases.createStringAttribute(db, commentCollection, "authorId", 50, true),
+        tablesDB.createStringColumn(db, commentCollection, "content", 10000, true),
+        tablesDB.createEnumColumn(db, commentCollection, "type", ["answer", "question"], true),
+        tablesDB.createStringColumn(db, commentCollection, "typeId", 50, true),
+        tablesDB.createStringColumn(db, commentCollection, "authorId", 50, true),
     ]);
     console.log("Comment Attributes Created");
 }
