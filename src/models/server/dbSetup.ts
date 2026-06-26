b4e8ac7b701af4ae333807401bb3e606f593852e
Naman Kumar <krnaman.2007@gmail.com>
2026-06-26 16:18:12 +0530

    edited dbSetup

--- src/models/server/dbSetup.ts
+++ src/models/server/dbSetup.ts
@@ -3,15 +3,15 @@ import createAnswerCollection from "./answer.collection";
 import createCommentCollection from "./comment.collection";
 import createQuestionCollection from "./question.collection";
 import createVoteCollection from "./vote.collection";
-import {databases} from "./config"
+import {tablesDB} from "./config"
 
 export default async function getOrCreateDB(){
     try {
-        await databases.get(db)
+        await tablesDB.get(db)
         console.log("Database connected")
     } catch (error) {
         try {
-            await databases.create(db,db)
+            await tablesDB.create(db,db)  //databaseId,name
             console.log("Database created")
             //create collections
             await Promise.all([
@@ -21,11 +21,10 @@ export default async function getOrCreateDB(){
                 createVoteCollection(),
             ])
             console.log("Collection created")
-            console.log("Database created")
         } catch (error) {
-            console.log("Error creating databases or collection",error)
+            console.log("Error creating tablesDB or collection",error)
         }
     }
-    return databases
+    return tablesDB
 }
 
