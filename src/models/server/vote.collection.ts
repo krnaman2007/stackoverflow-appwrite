cc8dd8915112e2571b7ec3eafaa7e164cbfec96c
Naman Kumar <krnaman.2007@gmail.com>
2026-06-26 15:58:38 +0530

    edited vote collection

--- src/models/server/vote.collection.ts
+++ src/models/server/vote.collection.ts
@@ -1,30 +1,35 @@
 import { Permission } from "node-appwrite";
 import { db, voteCollection } from "../name";
-import { databases } from "./config";
+import { tablesDB } from "./config";
 
 export default async function createVoteCollection() {
     // Creating Collection
-    await databases.createCollection(db, voteCollection, voteCollection, [
-        Permission.create("users"),
-        Permission.read("any"),
-        Permission.read("users"),
-        Permission.update("users"),
-        Permission.delete("users"),
-    ]);
+    await tablesDB.createTable(
+        db,
+        voteCollection,
+        voteCollection,
+        [
+            Permission.create("users"),
+            Permission.read("any"),
+            Permission.read("users"),
+            Permission.update("users"),
+            Permission.delete("users"),
+        ]
+    )
     console.log("Vote Collection Created");
 
     // Creating Attributes
     await Promise.all([
-        databases.createEnumAttribute(db, voteCollection, "type", ["question", "answer"], true),
-        databases.createStringAttribute(db, voteCollection, "typeId", 50, true),
-        databases.createEnumAttribute(
+        tablesDB.createEnumColumn(db, voteCollection, "type", ["question", "answer"], true),
+        tablesDB.createStringColumn(db, voteCollection, "typeId", 50, true),
+        tablesDB.createEnumColumn(
             db,
             voteCollection,
             "voteStatus",
             ["upvoted", "downvoted"],
             true
         ),
-        databases.createStringAttribute(db, voteCollection, "votedById", 50, true),
+        tablesDB.createStringColumn(db, voteCollection, "votedById", 50, true),
     ]);
     console.log("Vote Attributes Created");
 }
